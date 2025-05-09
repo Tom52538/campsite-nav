750a2058594fd24afbbdc9dd74332adea1ee4724
Tom52538 <tg1967mail@gmail.com>
2025-05-09 08:57:02 +0200

    new code

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.camping_osm_navi"
+    namespace = "com.example.camping_osm_navi" // Dein Namespace
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // <--- HIER DIE ÄNDERUNG
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.camping_osm_navi"
+        applicationId = "com.example.camping_osm_navi" // Deine Application ID
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -42,3 +42,5 @@ android {
 flutter {
     source = "../.."
 }
+
+// KEINE überflüssige Klammer mehr hier am Ende
